3921259367dc992d8c018274a754ae905f8317f1
vibin_intain <vibin.m@intainft.com>
2019-12-21 13:59:14 +0530

    minor chnage

--- Data Types/String.go
+++ Data Types/String.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var s string
-	s = "Let's learn Go"
+	s = "Let's learn Go" // defaults to ?
 	fmt.Println(s)
 	fmt.Println(len(s)) // The len built-in function returns the length of v, according to its type: Array: the number of elements in v.
 	fmt.Println()
