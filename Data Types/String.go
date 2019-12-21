b4c9c412e464531aa59142730dd4650a3de30ff1
vibin_intain <vibin.m@intainft.com>
2019-12-21 13:55:22 +0530

    Program to understand String in go

---
+++ Data Types/String.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	s = "Let's learn Go"
+	fmt.Println(s)
+	fmt.Println(len(s)) // The len built-in function returns the length of v, according to its type: Array: the number of elements in v.
+	fmt.Println()
+}
